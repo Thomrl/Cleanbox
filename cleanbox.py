61a656829a37d2e5dd09705ccba60c7da1c91bd9
Thomas Lauritzen <tommer.rl@live.dk>
2017-06-02 19:59:49 +0200

    Corrected code and comments
    I started out testing on another folder named inbox2. I thought I had changed all "inbox2" to "inbox" but I missed it for the installers.
    It should work perfectly now :)

--- cleanbox.py
+++ cleanbox.py
@@ -1,11 +1,11 @@
-# Just started learning Python, so the code could probably be a lot better :)
+# I just started learning Python, so the code could probably be a lot better :)
 import glob, os, shutil
 
 # Change directory to the inbox folder
 os.chdir("c:\\files\\settings\Thomas\\Desktop\\inbox")
 
     
-# Check if theres any file with the text "PUBG" in it and move that to youtube folder
+# Check every file in inbox for "PUBG" in the filename. If there is, proceed to next if statement.
 for file in glob.glob("*PUBG*"):
     # Check if there is a folder called Youtube if not make one.
     if not os.path.exists("c:\\files\\settings\Thomas\\Desktop\\inbox\\Youtube"):
@@ -16,7 +16,7 @@ for file in glob.glob("*PUBG*"):
     shutil.move(file, ".\\Youtube")
 
 
-# Check every file in inbox1, if a file ends with ".py" move it to ".\\scripts" which was created earlier
+# Check every file in inbox, if a file ends with ".py" proceed to next if statement.
 for file in os.listdir("c:\\files\\settings\Thomas\\Desktop\\inbox"):
     if file.endswith((".py")):
         # Check if there is a folder called scripts if not make one.
@@ -28,7 +28,7 @@ for file in os.listdir("c:\\files\\settings\Thomas\\Desktop\\inbox"):
         shutil.move(file, ".\\scripts")
 
 
-# Check every file in inbox1, if a file ends with ".jpg | .JPG | .jpeg | .png" move it to ".\\images" which was created earlier
+# Check every file in inbox, if a file ends with ".jpg | .JPG | .jpeg | .png proceed to next if statement.
 for file in os.listdir("c:\\files\\settings\Thomas\\Desktop\\inbox"):
     if file.endswith((".jpg", ".JPG", ".jpeg", ".png")):
         # Check if there is a folder called images if not make one.
@@ -40,12 +40,12 @@ for file in os.listdir("c:\\files\\settings\Thomas\\Desktop\\inbox"):
         shutil.move(file, ".\\images")
 
 
-# Check every file in inbox1, if a file ends with ".jpg | .JPG | .jpeg | .png" move it to ".\\images" which was created earlier
+# Check every file in inbox, if a file ends with ".exe" or ".msi" proceed to next if statement.
 for file in os.listdir("c:\\files\\settings\Thomas\\Desktop\\inbox"):
     if file.endswith((".exe", ".msi")):
         # Check if there is a folder called installers if not make one.
-        if not os.path.exists("c:\\files\\settings\Thomas\\Desktop\\inbox2\\installers"):
-            os.mkdir("c:\\files\\settings\Thomas\\Desktop\\inbox2\\installers")
+        if not os.path.exists("c:\\files\\settings\Thomas\\Desktop\\inbox\\installers"):
+            os.mkdir("c:\\files\\settings\Thomas\\Desktop\\inbox\\installers")
         #
         # prints status text and moves file to its destination
         print(file + " - has been moved to the folder: installers")
