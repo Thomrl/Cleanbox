c12853e2e6844b77e8dc4476f770b71a1b0ac473
Thomas Lauritzen <tommer.rl@live.dk>
2017-06-19 13:34:15 +0200

    Eliminating DRY code and fixing comments

--- cleanbox.py
+++ cleanbox.py
@@ -2,39 +2,29 @@
 import os, shutil, glob
 
 #Make sure the inbox folder is chosen.
-os.chdir("c:\\files\\settings\\Thomas\\Desktop\\Inbox\\")#Chosen directory
+os.chdir("c:\\files\\settings\\Thomas\\Desktop\\Inbox2\\")#Chosen directory
 
 #Look for "PUBG" in filenames.
-
-for file in glob.glob("*PUBG*"):
+for file in glob.glob("*PUBG*"):         #Goes through each file in the directory looking for "PUBG"
     if not os.path.exists(".\\Youtube"): #If "PUBG" is found check for "Youtube" folder
         os.mkdir(".\\Youtube")           #If folder is not found make it.
-    print(file + " --->> \"Youtube\"")
+    print(file + " --->> \"Youtube\"")   #Letting the user know which file is being moved to Youtube
     shutil.move(file, ".\\Youtube")      #Move file to the folder
-    
-#Look for image files like .jpg.
 
-for file in os.listdir():                #Goes through each file in the directory
-    if file.endswith((".jpg", ".JPG", ".jpeg", ".png", ".gif")): #Search for these files
-        if not os.path.exists(".\\Images"):#If the files are found check for "Images" folder
-            os.mkdir(".\\Images")        #If folder is not found make it.
-        print(file + " --->> \"Images\"")
-        shutil.move(file, ".\\Images")   #Move file to the folder
+#Files to look for.
+imgFiles = (".jpg", ".JPG", ".jpeg", ".png", ".gif") #Images
+pyFile = (".py")                                     #python files
+installFiles = (".exe", ".msi")                      #files thats typically used to install software.
 
-#Look for ".py" files
+#Function for the sorting code to eliminate DRY code.
+def sorting(fileType, location):
+    for file in os.listdir():                        #Goes through each file in the directory
+        if file.endswith(fileType):                  #Search for these files
+            if not os.path.exists(location):         #If the files are found check for "Images" folder
+                os.mkdir(location)                   #If folder is not found make it.
+            print(file + " --->> \""+location+"\"")  #Letting the user know what goes where.
+            shutil.move(file, location)              #Move file to the folder
 
-for file in os.listdir():
-    if file.endswith((".py")):
-        if not os.path.exists(".\\Scripts"):
-            os.mkdir(".\\Scripts")
-        print(file + " --->> \"Scripts\"")
-        shutil.move(file, ".\\Scripts")
-        
-#Look for ".exe" and ".msi" files
-
-for file in os.listdir():
-    if file.endswith((".exe", ".msi")):
-        if not os.path.exists(".\\Installers"):
-            os.mkdir(".\\Installers")
-        print(file + " --->> \"Installers\"")
-        shutil.move(file, ".\\Installers")
+sorting(imgFiles, "Images")           #Call sorting function in this case for the images
+sorting(pyFile, "Scripts")            #Call sorting function in this case for the python scripts
+sorting(installFiles, "Installers")   #Call sorting function in this case for the install files
